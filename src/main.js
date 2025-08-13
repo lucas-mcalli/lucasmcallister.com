1e40d8362cdc7a76c32672c014ceffe4d698fb76
lmcal <lmcalli124@gmail.com>
2025-08-12 23:55:29 -0400

    page1 progress

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import { animate, press } from "motion";
+import { animate, press, stagger } from "motion";
 press("#dark-mode-button", (element) => {
     animate(element, {scale: 0.9}, {type: "spring", stiffness: 1000})
-    return () => 
+    return () => {
         animate(element, {scale: 1.0}, {type: "spring", stiffness: 500})
+    }
 });
-animate("#home-arrow", {y: 50}, {duration: 1,  direction: "alternate", delay: 0.5});
+animate(
+  "#home-arrow",
+  { y: 40},
+  { delay: 0.5, duration: 1 }
+)
 
