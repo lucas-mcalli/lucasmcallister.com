0cdc369878af3a403958473cca5c92a04130c2d5
Lucas McAllister <lmcalli124@gmail.com>
2026-01-28 11:47:45 -0500

    Added analytics and cleaned console.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState, useRef } from "react";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App() {
 
@@ -30,6 +31,7 @@ export default function App() {
         <Route path="/" element={<Home isDark={isDark} toggleDarkMode={toggleDarkMode} isFirstLoad={isFirstLoad} isFirstHeroLoad={isFirstHeroLoad} />} />
         <Route path="/about" element={<About isDark={isDark} toggleDarkMode={toggleDarkMode} isFirstLoad={isFirstLoad} />} />
       </Routes>
+      <Analytics/>
     </BrowserRouter>
   );
 }
