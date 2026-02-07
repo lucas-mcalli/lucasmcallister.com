8ed1afd9bdbfc12669a5db69b17ce9ff35c02075
Lucas McAllister <lmcalli124@gmail.com>
2026-02-07 17:54:58 -0500

    Working on adding ReactBits background animations

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,19 @@
 import { useEffect, useRef } from "react";
 import { animate, stagger } from "motion";
+import Dither from './Dither.jsx';
+
+<div style={{ width: '100%', height: '1000px', position: 'relative' }}>
+  <Dither
+    waveColor={[0.5,0.5,0.5]}
+    disableAnimation={false}
+    enableMouseInteraction
+    mouseRadius={0.3}
+    colorNum={4}
+    waveAmplitude={0.3}
+    waveFrequency={3}
+    waveSpeed={0.05}
+  />
+</div>
 
 const Hero = ({ isFirstHeroLoad }) => {
 
