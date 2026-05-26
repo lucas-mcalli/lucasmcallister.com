cc18889414a037146cdc62fc03ed4f74de134f3d
Lucas McAllister <lmcalli124@gmail.com>
2026-05-26 16:11:58 -0400

    Removed breakpoint indicator

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -251,14 +251,6 @@ export default function Home({ isDark, toggleDarkMode, isFirstLoad, isFirstHeroL
       </section>
       <Footer />
       </div>
-        <div className="fixed bottom-4 left-4 z-50 bg-black text-white text-xs px-2 py-1 rounded">
-          <span className="sm:hidden">xs</span>
-          <span className="hidden sm:block md:hidden">sm</span>
-          <span className="hidden md:block lg:hidden">md</span>
-          <span className="hidden lg:block xl:hidden">lg</span>
-          <span className="hidden xl:block 2xl:hidden">xl</span>
-          <span className="hidden 2xl:block">2xl</span>
-        </div>
     </div>
   );
 }
